588f3749309fe80f3142c8c47de64277c4178470
toolbuilder <k_d_hudson@yahoo.com>
2020-06-29 16:29:04 -0400

    refactor: use @toolbuilder/rollup-plugin-commands instead of local version

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -2,14 +2,16 @@ import createTestPackageJson from 'rollup-plugin-create-test-package-json'
 import multiInput from 'rollup-plugin-multi-input'
 import relativeToPackage from 'rollup-plugin-relative-to-package'
 import createPackFile from '@toolbuilder/rollup-plugin-create-pack-file'
+import runCommands, { shellCommand } from '@toolbuilder/rollup-plugin-commands'
 import { tempPath } from './temp-path'
-import { runCommands, shellCommand } from './run-commands'
 
 const testPackageDir = tempPath('pkgtest')
 
 export default [
   {
+    // multi-input will turn glob into array of entry points
     input: ['test/**/*test.js'],
+    // output one file per entry point
     preserveModules: true,
     output: {
       format: 'es',
@@ -17,14 +19,19 @@ export default [
     },
     plugins: [
       multiInput(),
+      // convert relative imports to package imports
       relativeToPackage({
+        // where all package source files reside
         modulePaths: 'src/**/*.js'
       }),
       createTestPackageJson({
+        // fields plugin can't infer from packageJson and bundles
         testPackageJson: {
           scripts: {
+            // script to run generated unit tests
             test: 'tape -r esm test/**/*test.js | tap-nirvana'
           },
+          // test script dependencies
           devDependencies: {
             esm: '^3.2.25',
             tape: '^5.0.1',
@@ -33,11 +40,17 @@ export default [
         }
       }),
       createPackFile({
+        // create packfile (*.tgz) and move it to testPackageDir
         packCommand: 'pnpm pack'
       }),
-      runCommands(
-        shellCommand(`pnpm -C ${testPackageDir} install-test`)
-      )
+      runCommands({
+        commands: [
+          // install and run tests
+          // -C prevents package's path from being part of testPackage's path
+          // So that modules in package aren't resolved by accident
+          shellCommand(`pnpm -C ${testPackageDir} install-test`)
+        ]
+      })
     ]
   }
 ]
